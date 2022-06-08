780347db60927725f2c4d69ac36b0f87aa370b6f
AaryaCoder <84276676+AaryaCoder@users.noreply.github.com>
2022-06-08 17:47:06 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 status="";
-video="";
+objects=[];
 
 function setup(){
     canvas=createCanvas(380,380);
@@ -17,4 +17,35 @@ function modelLoaded(){
 }
 function draw(){
     image(video,0,0,380,380);
+
+    if(status != ""){
+        for(i=0; i<objects.length; i++){
+            percent= floor(objects[i].confidence *100);
+            text(objects[i].label +"" +percent + "%", objects[i].x + 15, objects[i].y +15);
+            fill("#f0523a");
+            stroke("#f0523a");
+            noFill();
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if(objects[i].label= object_name){
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("status_found").innerHTML=object_name+"found";
+                synth=window.speechSynthesis;
+                utterThis=new SpeechSynthesisUtterance(object_name+"found");
+                synth.speak(utterThis);
+            }
+            else{
+                document.getElementById("status_found").innerHTML=object_name +"object mentioned was not found :(";
+            }
+        }
+    }
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
 }
